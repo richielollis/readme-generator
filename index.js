9a4625adc3b04ac85e2f49e3378d13bc0277a8f2
richielollis <richiedlollis@gmail.com>
2022-09-13 21:28:54 -0400

    questions added

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Title:",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Description:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Installation:",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Usage:",
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Lisence:",
+    name: "lisence",
+    choices: ["MIT", "GLP", "LGPL", "Mozilla Public License", "none"],
+  },
+  {
+    type: "input",
+    message: "Contribution Guidelines:",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Test Instructions:",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "GitHub Username:",
+    name: "gitUserName",
+  },
+  {
+    type: "input",
+    message: "Email:",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
